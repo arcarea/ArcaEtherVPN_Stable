bb79358e073a5cbaf2b49365772b08065cd94f44
Chemie <43675937+chemie2472@users.noreply.github.com>
2021-10-17 16:29:32 +0900

    Update BuildAll.cmd

--- src/BuildAll.cmd
+++ src/BuildAll.cmd
@@ -3,8 +3,7 @@ SET BATCH_FILE_NAME=%0
 SET BATCH_DIR_NAME=%0\..
 SET NOW_TMP=%time:~0,2%
 SET NOW=%date:~0,4%%date:~5,2%%date:~8,2%_%NOW_TMP: =0%%time:~3,2%%time:~6,2%
-for /f "usebackq tokens=*" %i IN (`"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) DO @set vcdir=%i
-"%vcdir%\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 
 call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
